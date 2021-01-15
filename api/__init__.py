c5b97b518907be95bb42cc349aaff9841299838d
Roberto Sanchez <rg.sanchez.a@gmail.com>
2021-01-15 13:51:54 -0500

    Merge branch 'master' of E:\proyectos\CENACE-DISPONIBILIDAD\API_SistemaCentral with conflicts.

--- api/__init__.py
+++ api/__init__.py
@@ -16,10 +16,15 @@ project_path = os.path.dirname(api_path)
 sys.path.append(api_path)
 sys.path.append(project_path)
 
-from api.app_config import create_app
+
+""" global variables """
 from settings import initial_settings as init
+log = init.LogDefaultConfig("app_flask.log").logger
+from api.app_config import create_app
 
 app = create_app()
 
-""" global variables """
-log = init.LogDefaultConfig("app_flask.log").logger
+
+
+""" Note: import the models that migrate will track changes therefore don´t delete the next line """
+# from dto.sqlite_engine_handler import Users, Groups, Roles, Resources
